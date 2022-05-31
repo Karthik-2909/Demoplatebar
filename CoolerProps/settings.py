28301203e30ad38b63f5023929c72ddcc7caca00
Adiseshakarthik <91066416+Karthik-2909@users.noreply.github.com>
2022-05-31 13:25:19 +0530

    Update settings.py

--- CoolerProps/settings.py
+++ CoolerProps/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&5%1z92-9#&!v7t636tqdn@l6!z+f01)pb0+9l6(i!9%9q6owb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','platebardemo.herokuapp.com']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
